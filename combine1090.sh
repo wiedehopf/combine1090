680e51286277e48dd9cb97504061a3e19b64b49a
Matthias Wirth <matthias.wirth@gmail.com>
2019-02-01 00:09:34 +0100

    Some rudimentary rate limiting

--- combine1090.sh
+++ combine1090.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-opts="-d"
-retry=40
+opts="-T 90 -d"
+retry=15
 
 echo -n "Starting data redirection with socat for combine1090:    "
 date
@@ -12,13 +12,14 @@ trap "kill -2 0" SIGTERM
 
 for i in $SOURCES
 do
-	
 	for p in $PORTS
 	do
+		sleep .2
 		while true
 		do
 			socat $opts -u TCP:$i:$p TCP:$TARGET
 			sleep $retry
+			sleep $(($RANDOM%10)).$(($RANDOM%10))
 		done &
 	done
 done
