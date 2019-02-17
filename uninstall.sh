b4557bf25715c68d251d420cdd2efd19bd23ce6d
Matthias Wirth <matthias.wirth@gmail.com>
2019-02-17 15:51:13 +0100

    make sure we get all service files on uninstall

--- uninstall.sh
+++ uninstall.sh
@@ -3,8 +3,10 @@
 systemctl stop combine1090-dump combine1090
 systemctl disable combine1090-dump combine1090
 
-rm /etc/systemd/system/combine1090.service
-rm /etc/systemd/system/combine1090-dump.service
+rm -f /etc/systemd/system/combine1090.service
+rm -f /etc/systemd/system/combine1090-dump.service
+rm -f /lib/systemd/system/combine1090.service
+rm -f /lib/systemd/system/combine1090-dump.service
 rm /usr/local/bin/combine1090.sh
 lighty-disable-mod combine1090
 
