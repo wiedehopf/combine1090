17eeab50ed903bfbe7404acd494eaa26b27af487
Matthias Wirth <matthias.wirth@gmail.com>
2019-07-07 09:48:36 +0200

    some cleanups

--- combine1090.sh
+++ combine1090.sh
@@ -10,34 +10,37 @@ date
 trap "kill 0" SIGINT
 trap "kill -2 0" SIGTERM
 
-for i in $SOURCES
+for j in $TARGET
 do
-	for j in $TARGET
+	for i in $SOURCES
 	do
 		for p in $PORTS
 		do
 			sleep .2
 			while true
 			do
-				echo "Connecting: $j"
+				echo "Redirecting: SOURCE: $i:$p TARGET: $j"
 				socat $opts -u TCP:$i:$p TCP:$j
-				echo "Connection failed: $j"
+				echo "Lost Connection: SOURCE: $i:$p TARGET: $j"
 				sleep $retry
 				sleep $(($RANDOM%10)).$(($RANDOM%10))
 			done &
 		done
 	done
+	for i in $CUSTOM
+	do
+		sleep .2
+		while true
+		do
+			echo "Redirecting: SOURCE: $i TARGET: $j"
+			socat $opts -u TCP:$i TCP:$j
+			echo "Lost connection: SOURCE: $i TARGET: $j"
+			sleep $retry
+			sleep $(($RANDOM%10)).$(($RANDOM%10))
+		done &
+	done
 done
 
-for i in $CUSTOM
-do
-	
-	while true
-	do
-		socat $opts -u TCP:$i TCP:$TARGET
-		sleep $retry
-	done &
-done
 while true
 do
 	sleep 1024
